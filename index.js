1942cde6b6af16fbc048541c78fa4f89c5b8fb30
Gurottesque <a.bedoya3@utp.edu.co>
2024-07-04 22:41:58 -0500

    Update index.js

--- index.js
+++ index.js
@@ -8,6 +8,18 @@ const app = express();
 const port = 3000;
 
 app.get('/api/github/user/:username/repos', async (req, res) => {
+
+    const sort = req.query.sort;
+    const direction = req.query.direction;
+    if (sort && sort !== 'created' && sort !== 'updated' && sort !== 'pushed' && sort !== 'full_name') {
+        res.send('Error de parametros: sort tiene que ser uno de los siguientes valores:  `created`, `updated`, `pushed`, `full_name`')
+        return
+    }
+
+    if (direction && direction !== 'asc' && direction !== 'desc') {
+        res.send('Error de parametros: direction tiene que ser uno de los siguientes valores:  `asc`, `desc`')
+        return
+    }
     
     const params_api = {
         username: req.params.username,
@@ -21,6 +33,11 @@ app.get('/api/github/user/:username/repos', async (req, res) => {
 
 app.get('/api/github/user/:username', async (req, res) => {
     const include_repos = req.query.include_repos;
+    if (include_repos && include_repos !== 'false' && include_repos !== 'true' ) {
+        res.send('Error de parametros: include_repos tiene que ser un booleano (true) o (false)')
+        return
+    }
+
     const data = await api.getUser(req.params.username, include_repos)
 
     res.send(data)
